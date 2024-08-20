64c3a61cf2f73ab4687633265020f2d286597b62
brandondjango <brandonlockridge@gmail.com>
2024-08-20 12:11:53 -0400

    postgres changes and local db additions for keys

--- src/database_connector/postgres_connector.py
+++ src/database_connector/postgres_connector.py
@@ -1,34 +1,49 @@
 import psycopg2
 from psycopg2 import OperationalError
 
+class PostgresConnector:
 
-def create_connection():
-    try:
-        conn = psycopg2.connect(
-                host="localhost",
-                database="keys",
-                user="pyai",
-                password="Brandon123",
-                port=5432  # default port for PostgreSQL
-            )
-        return conn
-    except OperationalError as e:
-        print(f"The error '{e}' occurred")
-        return None
+    def create_connection(self):
+        try:
+            self.connection = psycopg2.connect(
+                    host="localhost",
+                    database="keys",
+                    user="pyai",
+                    password="Brandon123",
+                    port=5432  # default port for PostgreSQL
+                )
+        except OperationalError as e:
+            print(f"The error '{e}' occurred")
+            return None
 
-def execute_query(connection, query):
-    cursor = connection.cursor()
-    try:
-        cursor.execute(query)
-        result = cursor.fetchall()  # Fetch all rows from the last executed statement
-        for row in result:
-            print(row)
-        cursor.close()
-    except OperationalError as e:
-        print(f"The error '{e}' occurred")
-        return None
+    def execute_parameterized_select_query(self, query, params):
+        self.create_connection()
+        cursor = self.connection.cursor()
+        try:
+            cursor.execute(query, params)
+            result = cursor.fetchall()  # Fetch all rows from the last executed statement
+            cursor.close()
+            return result
+        except OperationalError as e:
+            cursor.close()
+            print(f"The error '{e}' occurred")
+            return None
 
-con = create_connection()
-query = 'SELECT * from keys;'
+    def execute_parameterized_insert_query(self, query, params):
+        self.create_connection()
+        cursor = self.connection.cursor()
+        try:
+            cursor.execute(query, params)
+            self.connection.commit()
+            cursor.close()
+        except OperationalError as e:
+            cursor.close()
+            print(f"The error '{e}' occurred")
+            return None
 
-execute_query(con, query)
+
+
+    def insert_into_keys_table(self, key_name, key_value):
+        self.execute_parameterized_insert_query(
+            "INSERT INTO keys(key_name, key_value) VALUES (%s, %s)", (key_name, key_value)
+        )
--- src/key_manager/key_manager.py
+++ src/key_manager/key_manager.py
@@ -3,7 +3,7 @@ import os
 from cryptography.fernet import Fernet
 import logging
 
-from src.config_util.config_util import ConfigUtil
+from src.database_connector.postgres_connector import PostgresConnector
 
 
 class KeyManager:
@@ -11,45 +11,41 @@ class KeyManager:
     logging.basicConfig(level=logging.INFO)
     log = logging.getLogger(__name__)
 
-    def __init__(self):
-        if not (os.getenv('CIPHER_KEY') == 'None'):
-            KeyManager.log.info("CIPHER_KEY exists")
-            return
-        else:
-            KeyManager.log.info("CIPHER_KEY does not exist, creating new key")
-
     def setup_local_encryption(self):
         key = Fernet.generate_key()
         os.environ['CIPHER_KEY'] = key.decode()
 
-    def setup_chatgpt_key(self, local=True):
+    def setup_chatgpt_key(self):
         try:
-            if local:
-                if('CHATGPT_KEY' in os.environ()):
-                    KeyManager.log.info("Local CHATGPT_KEY exists")
-                    return
-            else:
-                if not (ConfigUtil.get_current_configs("chatgpt")['chat_gpt_key'] == ''):
-                    KeyManager.log.info("Config chat_gpt_key exists")
-                    return
-        except TypeError:
-            print("No ChatGPT Key present, creating local key")
-
-        cipher = Fernet(os.getenv('CIPHER_KEY'))
-
-        #promt for chat_gpt_key
-        chat_gpt_key_encrypted = cipher.encrypt(input("Enter your ChatGPT key:").encode())
-        if local:
-            os.environ['CHATGPT_KEY'] = chat_gpt_key_encrypted.decode()
-            KeyManager.log.info("Local CHATGPT_KEY saved")
-        else:
-             ConfigUtil.add_config_value('chat_gpt_key', chat_gpt_key_encrypted, "chatgpt")
+            db_connector = PostgresConnector()
+            os.environ['CHATGPT_KEY'] = db_connector.execute_parameterized_select_query("select key_value from keys where key_name = \'chatgpt_key\'", ())[0][0]
+        except:
+            raise Exception("Could not fetch ChatGPT key")
 
     def get_chat_gpt_key(self):
-        KeyManager.setup_chatgpt_key()
-        return
+        if(os.environ.get('CHATGPT_KEY', None) == None):
+            self.setup_chatgpt_key()
+        return os.environ['CHATGPT_KEY']
 
+    def setup_cipher_key(self):
+        key = Fernet.generate_key()
+        os.environ['CIPHER_KEY'] = key.decode()
+        try:
+            db_connector = PostgresConnector()
+            db_connector.insert_into_keys_table( "cipher_key", os.environ['CIPHER_KEY'])
+        except Exception as e:
+            raise Exception("Error creating CIPHER_KEY:" + str(e))
 
+    def get_cipher_key(self):
+        db_connector = PostgresConnector()
+        os.environ['CIPHER_KEY'] = db_connector.execute_parameterized_select_query("select key_value from keys where key_name = \'cipher_key\'", ())[0][0]
+        if(os.environ.get('CIPHER_KEY', None) == None):
+            self.setup_cipher_key()
+        return os.environ['CIPHER_KEY']
+
+    def get_all_keys(self):
+        db_connector = PostgresConnector()
+        print(db_connector.execute_parameterized_select_query("select * from keys", ()))
 
 
 
