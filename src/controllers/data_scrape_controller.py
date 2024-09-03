08d315ea99c692a147c1907eec109d2980af6704
brandondjango <brandonlockridge@gmail.com>
2024-09-02 21:09:11 -0400

    next up switch to environment variables

--- src/controllers/data_scrape_controller.py
+++ src/controllers/data_scrape_controller.py
@@ -4,21 +4,34 @@ import yaml
 from flask import Flask, request, jsonify
 
 from src.config_util.config_util import ConfigUtil
+from src.data_scraper.match_scraper import MatchScraper
 
 app = Flask(__name__)
 
-@app.route('/data_scrape', methods=['POST'])
+@app.route('/scrape_match', methods=['POST'])
 def api_endpoint():
     data = request.json
+
+    if data.get("match_id") is None:
+        response = {
+            'status': 'Failed',
+            'message': "Match Id not provided: "
+        }
+        return jsonify(response), 500
+
+    match_id = data.get("match_id")
+
+    MatchScraper.scrape_match(match_id)
+
     # Process the data as needed
     response = {
         'status': 'success',
-        'data_received': data
+        'message': "Scraped match with id: " + data.get(match_id)
     }
     return jsonify(response), 200
 
 @app.route('/db_setup', methods=['POST'])
-def db_endpoint():
+def db_setup_endpoint():
     data = request.json
     db_pass = data.get('db_pass')
 
